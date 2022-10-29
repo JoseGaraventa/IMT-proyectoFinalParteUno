f567e3d7bb9f258d24e17d4b725b5280e19692a5
Jose Garaventa <josemanuelgaraventa@hotmail.com>
2022-10-29 08:08:26 -0300

    clase 1 react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {Curiosidades} from "./data"  // se importa el array
 
 function App() {
+
+  const bienvenida = "Bienvenidos a React!";
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //JSX
+    <div className = "App">
+    <header className = "App-header" >
+
+    <h1 > {bienvenida} </h1>   
+    <h2>Curiosidades</h2>
+    <ul>
+      {Curiosidades.map(curiosidad=>{
+        return <li key={curiosidad.id}>{curiosidad.descripcion}</li>  // Las key son un requerimiento de js para distinguir a cada elemento de la lista
+      })}
+    </ul> 
+
+    <h2>Caracteristicas</h2>
+
+    <ul>
+    <li>Es componible (hecho por componentes)</li>
+    <li>Es declarativo a diferencia de JS</li>
+    <li>Es una habilidad laboral dentro del IT</li>
+    <li>Es mantenido por un equipo competente</li>
+    </ul>
+    
+    
+    <p>
+      <button onClick={alert("Hola!")}> Hola </button>
+    </p>
+    </header> 
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const Curiosidades =[
+    {
+      id: 1,
+      descripcion:"Fue lanzado en 2013"
+    },
+    {
+      id:2,
+      descripcion:"Es mantenido por el equipo de facebook"
+    },
+    {
+      id:3,
+      descripcion:"Es usado en el desarrollo de miles de apps, incluidas mobile"
+    }
+    ];
+
+    //export default {Curiosidades};
